d596377f6452389e6cc69dcd5438975483358adb
Clark Richards <richardsc@users.noreply.github.com>
2021-05-19 18:03:01 -0300

    Fix issues arising from processing DFO COGG SeaExplorer (GPCTD+FLBBCD+AROD). (#10)
    * changes to accomodate DFO sx that does have an AROD_FT
    
    * fix up
    
    * another fix up
    
    * fix issue of unsorted times crashing open_mfdataset
    
    * commit to simply using glider_name in the filename creation
    
    * Remove raw_to_L1timeseries and clean up example.
    
    Also renamed raw_toL1timeseries_raw to raw_to_L0timeseries and fixed the
    example-seaexplorer to be consistent.
    
    Cleaned up the "don't write small nc file" logic to properly display the
    message (only display it if it *doesn't* write the file).

--- example-seaexplorer/process_deploymentRealTime.py
+++ example-seaexplorer/process_deploymentRealTime.py
@@ -10,7 +10,7 @@ sourcedir = '~alseamar/Documents/SEA035/000012/000012/C-Csv/*'
 rawdir  = './realtime_raw/'
 rawncdir     = './realtime_rawnc/'
 deploymentyaml = './deploymentRealtime.yml'
-l1tsdir    = './L1-timeseries/'
+l0tsdir    = './L0-timeseries/'
 profiledir = './L1-profiles/'
 griddir    = './L2-gridfiles/'
 plottingyaml = './plottingconfig.yml'
@@ -21,7 +21,7 @@ if 0:
     os.system('rsync -av ' + sourcedir + ' ' + rawdir)
 
 # clean last processing...
-os.system('rm ' + rawncdir + '* ' + l1tsdir + '* ' + profiledir + '* ' +
+os.system('rm ' + rawncdir + '* ' + l0tsdir + '* ' + profiledir + '* ' +
           griddir + '* ')
 
 
@@ -32,7 +32,7 @@ if 1:
     seaexplorer.merge_rawnc(rawncdir, rawncdir, deploymentyaml, kind='sub')
 
         # Make level-1 timeseries netcdf file from th raw files...
-    outname = seaexplorer.raw_to_L1timeseries_raw(rawncdir, l1tsdir, deploymentyaml, kind='sub')
+    outname = seaexplorer.raw_to_L0timeseries(rawncdir, l0tsdir, deploymentyaml, kind='sub')
     ncprocess.extract_L1timeseries_profiles(outname, profiledir, deploymentyaml)
     outname2 = ncprocess.make_L2_gridfiles(outname, griddir, deploymentyaml)
 
