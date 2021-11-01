64f52bb34ff6d52299d916115e28b7a42dc5bef8
Callum Rollo <c.rollo@outlook.com>
2021-11-01 14:21:42 +0000

    adding seperate test data to avoid conflict with demo data tests

--- tests/test_seaexplorer.py
+++ tests/test_seaexplorer.py
@@ -1,24 +1,25 @@
 import xarray as xr
-import logging
 from pathlib import Path
 import sys
-
+import os
+print(os.getcwd())
+os.system('rm tests/data/realtime_rawnc/*')
 library_dir = Path(__file__).parent.parent.absolute()
 sys.path.append(str(library_dir))
 import pyglider.seaexplorer as seaexplorer
 
 
 def test__outputname():
-    fnout, filenum = seaexplorer._outputname('example-seaexplorer/realtime_raw/sea035.12.pld1.sub.36',
-                            'example-seaexplorer/realtime_rawnc/')
-    assert fnout == 'example-seaexplorer/realtime_rawnc/sea035.0012.pld1.sub.0036.nc'
+    fnout, filenum = seaexplorer._outputname('tests/data/realtime_raw/sea035.12.pld1.sub.36',
+                            'tests/data/realtime_rawnc/')
+    assert fnout == 'tests/data/realtime_rawnc/sea035.0012.pld1.sub.0036.nc'
     assert filenum == 36
    
    
 def test__needsupdating():
     ftype = 'pld1'
-    fin = 'example-seaexplorer/realtime_raw/sea035.12.pld1.sub.36'
-    fout = 'example-seaexplorer/realtime_rawnc/sea035.0012.pld1.sub.0036.nc'
+    fin = 'tests/data/realtime_raw/sea035.12.pld1.sub.36'
+    fout = 'tests/data/realtime_rawnc/sea035.0012.pld1.sub.0036.nc'
     result_badpath = seaexplorer._needsupdating(ftype, fin, 'baz')
     result_goodpath = seaexplorer._needsupdating(ftype, fin, fout)
     assert result_badpath is True
@@ -27,21 +28,21 @@ def test__needsupdating():
 
 def test_raw_to_rawnc():
     # Default settings on a clean folder
-    result_default = seaexplorer.raw_to_rawnc('example-seaexplorer/realtime_raw/',
-                             'example-seaexplorer/realtime_rawnc/',
+    result_default = seaexplorer.raw_to_rawnc('tests/data/realtime_raw/',
+                             'tests/data/realtime_rawnc/',
                              None)
     # Test the reprocess flag works
-    result_reprocess = seaexplorer.raw_to_rawnc('example-seaexplorer/realtime_raw/',
-                                              'example-seaexplorer/realtime_rawnc/',
+    result_reprocess = seaexplorer.raw_to_rawnc('tests/data/realtime_raw/',
+                                              'tests/data/realtime_rawnc/',
                                               None,
                                               incremental=False)
     # Check that reprocessing not preformed by default
-    result_no_new_files = seaexplorer.raw_to_rawnc('example-seaexplorer/realtime_raw/',
-                             'example-seaexplorer/realtime_rawnc/',
+    result_no_new_files = seaexplorer.raw_to_rawnc('tests/data/realtime_raw/',
+                             'tests/data/realtime_rawnc/',
                              None)
     # Reject all payload files with fewer than 10000 lines
-    result_strict = seaexplorer.raw_to_rawnc('example-seaexplorer/realtime_raw/',
-                                              'example-seaexplorer/realtime_rawnc/',
+    result_strict = seaexplorer.raw_to_rawnc('tests/data/realtime_raw/',
+                                              'tests/data/realtime_rawnc/',
                                              None,
                                              incremental=False,
                                              min_samples_in_file=10000)
@@ -52,12 +53,12 @@ def test_raw_to_rawnc():
 
 
 def test_merge_rawnc():
-    result_default = seaexplorer.merge_rawnc('example-seaexplorer/realtime_rawnc/',
-                                             'example-seaexplorer/realtime_rawnc/',
+    result_default = seaexplorer.merge_rawnc('tests/data/realtime_rawnc/',
+                                             'tests/data/realtime_rawnc/',
                                              'example-seaexplorer/deploymentRealtime.yml')
 
-    result_sub = seaexplorer.merge_rawnc('example-seaexplorer/realtime_rawnc/',
-                                             'example-seaexplorer/realtime_rawnc/',
+    result_sub = seaexplorer.merge_rawnc('tests/data/realtime_rawnc/',
+                                             'tests/data/realtime_rawnc/',
                                              'example-seaexplorer/deploymentRealtime.yml',
                                              kind='sub')
     assert result_default is False
@@ -65,7 +66,12 @@ def test_merge_rawnc():
 
 
 def test__interp_gli_to_pld():
-    pass
+    # function should interpolate values from the glider dataset to sampling frequency of payload dataset
+    glider = xr.open_dataset('tests/data/realtime_rawnc/sea035.0012.gli.sub.0036.nc')
+    ds = xr.open_dataset('tests/data/realtime_rawnc/sea035.0012.pld1.sub.0036.nc')
+    val = glider.Pitch
+    pitch_interp = seaexplorer._interp_gli_to_pld(glider, ds, val, None)
+    assert len(pitch_interp) == len(ds.time)
 
 
 def test__interp_pld_to_pld():
