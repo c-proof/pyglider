4b604275725c0e19f5251d2ff9bf001b9c1b1aeb
Callum Rollo <c.rollo@outlook.com>
2022-05-09 16:43:19 +0200

    adjust test oxygen value to match new routine

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -31,4 +31,4 @@ def test_oxygen_concentration_correction_variables_unchanged(var):
 def test_oxygen_concentration_correction():
     oxygen_difference = np.nanmean(ds_out['oxygen_concentration'].values) \
                         - np.nanmean(ds_unchanged['oxygen_concentration'].values)
-    assert np.abs(oxygen_difference + 32.373) < 1e-2
+    assert np.abs(oxygen_difference + 32.328) < 1e-2
