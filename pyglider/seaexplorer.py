06d333030f7e55c099965c4ecbb93ac258e35c1c
cproof <cproof@cproof.arbutus>
2025-05-23 02:34:50 +0000

    DOC: add proper docstring

--- pyglider/seaexplorer.py
+++ pyglider/seaexplorer.py
@@ -343,7 +343,41 @@ def raw_to_timeseries(
     deadreckon=False,
 ):
     """
-    A little different than above, for the 4-file version of the data set.
+    Convert raw seaexplorer data to a timeseries netcdf file.
+
+    Parameters
+    ----------
+    indir : str
+        Directory with the raw files are kept.
+    outdir : str
+        Directory to write the matching ``*.nc`` files.
+    deploymentyaml : str
+        YAML text file with deployment information for this glider.
+    kind : 'raw' or 'sub'
+        The type of data to process.  'raw' is the full resolution data, 'sub'
+        is the sub-sampled data.  The default is 'raw'.  Note that realtime data is
+        typically sub-sampled.
+    profile_filt_time : float
+        Time in seconds to use for filtering the profiles.  Default is 100.
+    profile_min_time : float
+        Minimum time in seconds for a profile to be considered a valid profile.
+        Default is 300.
+    maxgap : float
+        Maximum gap in seconds to interpolate over.  Default is 10.
+    interpolate : bool
+        If *True*, interpolate the data to fill in gaps.  Default is False.
+    fnamesuffix : str
+        Suffix to add to the output file name.  Default is ''.
+    deadreckon : bool
+        If *True* use the dead reckoning latitude and longitude data from the glider.  Default
+        is *False*, and latitude and longitude are linearly interpolated between sueface fixes.
+        *False* is the default, and recommended to avoid a-physical underwater jumps.
+
+    Returns
+    -------
+    outname : str
+        Name of the output netcdf file.
+
     """
 
     deployment = utils._get_deployment(deploymentyaml)
