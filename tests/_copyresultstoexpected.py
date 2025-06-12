f828d7512f83da12a017dd38a352d6b7e1eea082
cproof <cproof@cproof.arbutus>
2025-06-12 23:30:10 +0000

    FIX

--- tests/_copyresultstoexpected.py
+++ tests/_copyresultstoexpected.py
@@ -5,7 +5,7 @@ If we run the tests and decide we want all the test results to be copied over, t
 from shutil import copy
 
 todo = {
-    'example-data/example-seaexplorer/L0-timeseries/dfo-eva035-20190718.nc': 'expected/example-seaexplorer/L0-timeseries',
+    'example-data/example-seaexplorer/L0-timeseries-test/dfo-eva035-20190718.nc': 'expected/example-seaexplorer/L0-timeseries',
     'example-data/example-seaexplorer-raw/L0-timeseries-test/dfo-bb046-20200908.nc': 'expected/example-seaexplorer-raw/L0-timeseries',
     'example-data/example-slocum/L0-timeseries/dfo-rosie713-20190615.nc': 'expected/example-slocum/L0-timeseries'
 }
