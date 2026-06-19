1f5fe4a979b82a669dccabdac88d5b00fb0ad2d1
Jody Klymak <jklymak@gmail.com>
2026-06-19 15:47:49 -0700

    CI: fix pyproject.toml

--- pyglider/_version.py
+++ pyglider/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.0.8.dev98+g6fb91879b.d20260619'
-__version_tuple__ = version_tuple = (0, 0, 8, 'dev98', 'g6fb91879b.d20260619')
+__version__ = version = '0.0.8.dev99+ga77b888fd.d20260619'
+__version_tuple__ = version_tuple = (0, 0, 8, 'dev99', 'ga77b888fd.d20260619')
 
-__commit_id__ = commit_id = 'g6fb91879b'
+__commit_id__ = commit_id = 'ga77b888fd'
